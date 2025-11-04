b75783ee90ff818f2fc25239db3b94994e1a3f72
Sergey Stupin <stupin.sergey@gmail.com>
2025-11-04 17:47:00 +0100

    add init_storage function with test

--- src/nemory/embeddings/impl/storage.py
+++ src/nemory/embeddings/impl/storage.py
@@ -1,10 +1,18 @@
 from importlib.resources import read_text
+from pathlib import Path
 
 import duckdb
 
+def init_storage(database_path: str | Path):
+    with connect(database_path, read_only=False) as connection:
+        vss_extension_sql = load_vss_extension_sql()
+        schemq_sql = load_embeddings_schema_sql()
+        connection.execute(vss_extension_sql)
+        connection.execute(schemq_sql)
 
-def connect(database_path: str):
-    return duckdb.connect(database=database_path, read_only=True)
+
+def connect(database_path: str | Path, read_only=True):
+    return duckdb.connect(database=database_path, read_only=read_only)
 
 
 def load_embeddings_schema_sql():
--- src/nemory/embeddings/tests/test_embeddings.py
+++ src/nemory/embeddings/tests/test_embeddings.py
@@ -1,4 +1,11 @@
-from nemory.embeddings.impl.storage import load_embeddings_schema_sql, load_vss_extension_sql
+from pathlib import Path
+
+from nemory.embeddings.impl.storage import (
+    load_embeddings_schema_sql,
+    load_vss_extension_sql,
+    init_storage,
+    connect
+)
 
 
 def test_embeddings_schema_loaded():
@@ -7,3 +14,18 @@ def test_embeddings_schema_loaded():
 
 def test_vss_extension_loaded():
     assert load_vss_extension_sql()
+
+
+def test_init_duckdb(tmp_path: Path):
+    db_file = tmp_path / "test_db.duckdb"
+    init_storage(db_file)
+    with connect(db_file) as connection:
+        tables = connection.sql(
+            """
+            SELECT table_name
+            FROM information_schema.tables
+            WHERE table_catalog = ?
+            """,
+            params=["test_db"]
+        ).fetchall()
+        assert 'embeddings' in [table[0] for table in tables]
