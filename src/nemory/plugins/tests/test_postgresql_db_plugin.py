e92fc41a3a67d3c47dec63340935b7b1b52ea9dc
Julien Arzul <julien.arzul@jetbrains.com>
2025-11-12 16:58:38 +0100

    Add the divide_into_chunks implementation of the Postgres DB Plugin

--- src/nemory/plugins/tests/test_postgresql_db_plugin.py
+++ src/nemory/plugins/tests/test_postgresql_db_plugin.py
@@ -1,9 +1,13 @@
+from datetime import datetime
 from typing import Any, Mapping
 
 import psycopg
 import pytest
+from pytest_unordered import unordered
 from testcontainers.postgres import PostgresContainer  # type: ignore
 
+from nemory.pluginlib.build_plugin import BuildExecutionResult, EmbeddableChunk
+from nemory.plugins.databases.database_chunker import DatabaseColumnChunkContent, DatabaseTableChunkContent
 from nemory.plugins.databases.databases_types import (
     DatabaseCatalog,
     DatabaseColumn,
@@ -36,7 +40,7 @@ def postgres_container_with_columns(postgres_container):
     return postgres_container
 
 
-def test_postgres_container(postgres_container_with_columns: PostgresContainer):
+def test_postgres_plugin_execute(postgres_container_with_columns: PostgresContainer):
     plugin = PostgresqlDbPlugin()
 
     config_file = _create_config_file_from_container(postgres_container_with_columns)
@@ -68,6 +72,86 @@ def test_postgres_container(postgres_container_with_columns: PostgresContainer):
                 ],
             )
         ]
+    )
+
+
+def test_postgres_plugin_divide_into_chunks():
+    plugin = PostgresqlDbPlugin()
+
+    input = BuildExecutionResult(
+        id="id",
+        name="name",
+        type="databases/postgres",
+        description=None,
+        version=None,
+        executed_at=datetime.now(),
+        result=DatabaseIntrospectionResult(
+            catalogs=[
+                DatabaseCatalog(
+                    name="test",
+                    schemas=[
+                        DatabaseSchema(
+                            name="public",
+                            tables=[],
+                        ),
+                        DatabaseSchema(
+                            name="custom",
+                            tables=[
+                                DatabaseTable(
+                                    name="test",
+                                    description="best table",
+                                    columns=[
+                                        DatabaseColumn(name="id", type="int4", nullable=False),
+                                        DatabaseColumn(name="name", type="varchar", nullable=True),
+                                    ],
+                                    samples=[],
+                                )
+                            ],
+                        ),
+                    ],
+                )
+            ]
+        ),
+    )
+
+    chunks = plugin.divide_result_into_chunks(input)
+
+    assert len(chunks) == 3
+    assert chunks == unordered(
+        EmbeddableChunk(
+            embeddable_text="test - best table",
+            content=DatabaseTableChunkContent(
+                catalog_name="test",
+                schema_name="custom",
+                table=DatabaseTable(
+                    name="test",
+                    description="best table",
+                    columns=[
+                        DatabaseColumn(name="id", type="int4", nullable=False),
+                        DatabaseColumn(name="name", type="varchar", nullable=True),
+                    ],
+                    samples=[],
+                ),
+            ),
+        ),
+        EmbeddableChunk(
+            embeddable_text="id",
+            content=DatabaseColumnChunkContent(
+                catalog_name="test",
+                schema_name="custom",
+                table_name="test",
+                column=DatabaseColumn(name="id", type="int4", nullable=False),
+            ),
+        ),
+        EmbeddableChunk(
+            embeddable_text="name",
+            content=DatabaseColumnChunkContent(
+                catalog_name="test",
+                schema_name="custom",
+                table_name="test",
+                column=DatabaseColumn(name="name", type="varchar", nullable=True),
+            ),
+        ),
     )
 
 
