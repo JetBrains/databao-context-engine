7d01e20ce4d21f8c74bee1608e89f32eec7a22e9
Julien Arzul <julien.arzul@jetbrains.com>
2025-11-28 10:47:49 +0100

    Error handling: Only print the error message when an exception is raised and the command was not in verbose mode

--- src/nemory/main.py
+++ src/nemory/main.py
@@ -1,8 +1,18 @@
+import logging
+
 from nemory.cli.commands import nemory
+
+logger = logging.getLogger(__name__)
 
 
 def main() -> None:
-    nemory(obj={})
+    try:
+        nemory(obj={})
+    except Exception as e:
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.exception(e)
+        else:
+            logger.error(str(e))
 
 
 if __name__ == "__main__":
