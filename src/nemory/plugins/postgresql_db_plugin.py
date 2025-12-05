d37683b5bb7449a81cbbcb6cf2ba7f34bef53593
Julien Arzul <julien.arzul@jetbrains.com>
2025-12-05 10:55:10 +0100

    Removed postgresql plugin's custom config file properties, to let it use its config type

--- src/nemory/plugins/postgresql_db_plugin.py
+++ src/nemory/plugins/postgresql_db_plugin.py
@@ -1,4 +1,3 @@
-from nemory.pluginlib.config_properties import ConfigPropertyDefinition
 from nemory.plugins.base_db_plugin import BaseDatabasePlugin
 from nemory.plugins.databases.postgresql_introspector import PostgresConfigFile, PostgresqlIntrospector
 
@@ -11,19 +10,3 @@ class PostgresqlDbPlugin(BaseDatabasePlugin[PostgresConfigFile]):
 
     def __init__(self):
         super().__init__(PostgresqlIntrospector())
-
-    def get_config_file_properties(self) -> list[ConfigPropertyDefinition]:
-        return [
-            ConfigPropertyDefinition(
-                property_key="connection",
-                required=True,
-                property_type=None,
-                nested_properties=[
-                    ConfigPropertyDefinition(property_key="host", required=True, default_value="localhost"),
-                    ConfigPropertyDefinition(property_key="port", required=False, property_type=int),
-                    ConfigPropertyDefinition(property_key="database", required=False),
-                    ConfigPropertyDefinition(property_key="user", required=False),
-                    ConfigPropertyDefinition(property_key="password", required=False),
-                ],
-            )
-        ]
