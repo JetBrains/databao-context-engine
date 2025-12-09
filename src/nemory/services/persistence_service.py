9ff76c77107cdf10994c56451436fb45a3ea9925
Julien Arzul <julien.arzul@jetbrains.com>
2025-12-09 15:34:59 +0100

    Compute the display_text of a chunk by serialising it to YAML

--- src/nemory/services/persistence_service.py
+++ src/nemory/services/persistence_service.py
@@ -2,13 +2,11 @@ from collections.abc import Sequence
 
 import duckdb
 
-from nemory.pluginlib.build_plugin import EmbeddableChunk
-from nemory.serialisation.yaml import to_yaml_string
-from nemory.storage.models import ChunkDTO
-from nemory.storage.repositories.embedding_repository import EmbeddingRepository
-from nemory.storage.repositories.chunk_repository import ChunkRepository
-from nemory.storage.transaction import transaction
 from nemory.services.models import ChunkEmbedding
+from nemory.storage.models import ChunkDTO
+from nemory.storage.repositories.chunk_repository import ChunkRepository
+from nemory.storage.repositories.embedding_repository import EmbeddingRepository
+from nemory.storage.transaction import transaction
 
 
 class PersistenceService:
@@ -39,16 +37,19 @@ class PersistenceService:
             for chunk_embedding in chunk_embeddings:
                 chunk_dto = self.create_chunk(
                     datasource_run_id=datasource_run_id,
-                    chunk=chunk_embedding.chunk,
+                    embeddable_text=chunk_embedding.chunk.embeddable_text,
+                    display_text=chunk_embedding.display_text,
                     generated_description=chunk_embedding.generated_description,
                 )
                 self.create_embedding(table_name=table_name, chunk_id=chunk_dto.chunk_id, vec=chunk_embedding.vec)
 
-    def create_chunk(self, *, datasource_run_id: int, chunk: EmbeddableChunk, generated_description: str) -> ChunkDTO:
+    def create_chunk(
+        self, *, datasource_run_id: int, embeddable_text: str, display_text: str, generated_description: str
+    ) -> ChunkDTO:
         return self._chunk_repo.create(
             datasource_run_id=datasource_run_id,
-            embeddable_text=chunk.embeddable_text,
-            display_text=to_yaml_string(chunk.content),
+            embeddable_text=embeddable_text,
+            display_text=display_text,
             generated_description=generated_description,
         )
 
