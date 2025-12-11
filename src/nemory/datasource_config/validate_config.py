fba6b16c48fae6cd44d3b5c136b30614595fdf7f
Julien Arzul <julien.arzul@jetbrains.com>
2025-12-11 17:25:59 +0100

    NEM-279: [Validate Datasources] Output both the list of errors and a summary of the status for each datasource

--- src/nemory/datasource_config/validate_config.py
+++ src/nemory/datasource_config/validate_config.py
@@ -43,24 +43,40 @@ def validate_datasource_config(project_dir: Path, *, datasource_config_files: li
 
     results = _validate_datasource_config(project_dir, datasource_config_files=datasource_config_files)
 
-    resuls_as_string = (
-        os.linesep.join(
-            [
-                f"{datasource_path}: {validation_result.format(show_summary_only=len(results) > 1)}"
-                for datasource_path, validation_result in results.items()
-            ]
-        )
-        if results
-        else "No datasource found"
-    )
-    logger.info(f"Validation complete: {os.linesep}{resuls_as_string}")
+    if len(results) > 0:
+        valid_datasources = {
+            key: value for key, value in results.items() if value.validation_status == ValidationStatus.VALID
+        }
+        invalid_datasources = {
+            key: value for key, value in results.items() if value.validation_status == ValidationStatus.INVALID
+        }
+        unknown_datasources = {
+            key: value for key, value in results.items() if value.validation_status == ValidationStatus.UNKNOWN
+        }
 
-    if len(results) > 1 and any(
-        result for result in results.values() if result.validation_status == ValidationStatus.INVALID
-    ):
-        logger.info(
-            'To get more details about the invalid configuration, try re-running this command specifying only one datasource to validate.\n e.g: nemory datasource validate "my-source-folder/my-config-name.yaml"'
+        # Print all errors
+        for datasource_path, validation_result in invalid_datasources.items():
+            logger.info(
+                f"Error for datasource {datasource_path}:{os.linesep}{validation_result.full_message}{os.linesep}"
+            )
+
+        results_summary = (
+            os.linesep.join(
+                [
+                    f"{datasource_path}: {validation_result.format(show_summary_only=True)}"
+                    for datasource_path, validation_result in results.items()
+                ]
+            )
+            if results
+            else "No datasource found"
         )
+
+        logger.info(
+            f"Validation completed with {len(valid_datasources)} valid datasource(s) and {len(invalid_datasources) + len(unknown_datasources)} invalid (or unknown status) datasource(s)"
+            f"{os.linesep}{results_summary}"
+        )
+    else:
+        logger.info("No datasource found")
 
 
 def _validate_datasource_config(
