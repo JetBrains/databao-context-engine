5e2be35a3bf0ee40da50c80716553d0a710c1c11
Julien Arzul <julien.arzul@jetbrains.com>
2025-12-16 16:38:35 +0100

    NEM-222: Allow to resolve environment variables in config files, using Jinja templates and a custom env_var function

--- src/nemory/templating/renderer.py
+++ src/nemory/templating/renderer.py
@@ -7,11 +7,30 @@ class DceTemplateError(Exception):
     pass
 
 
+class UnknownEnvVarTemplateError(DceTemplateError):
+    pass
+
+
 def render_template(source: str) -> str:
     env = SandboxedEnvironment()
 
     try:
-        return env.from_string(source=str(source)).render()
+        return env.from_string(source=str(source)).render(env_var=resolve_env_var)
     except Exception as e:
+        if isinstance(e, DceTemplateError):
+            raise e
+
         # Wraps any Jinja exception into our own
         raise DceTemplateError(f"Error rendering template:{os.linesep}{str(e)}") from e
+
+
+def resolve_env_var(env_var: str, default: str | None = None) -> str:
+    if env_var in os.environ:
+        return os.environ[env_var]
+
+    if default is not None:
+        return default
+
+    raise UnknownEnvVarTemplateError(
+        f"Error in template. The environment variable {env_var} is missing and no default was provided"
+    )
--- tests/templating/test_renderer.py
+++ tests/templating/test_renderer.py
@@ -1,6 +1,9 @@
+import os
+
+import pytest
 import yaml
 
-from nemory.templating.renderer import render_template
+from nemory.templating.renderer import UnknownEnvVarTemplateError, render_template
 
 
 def test_render_template__plain_yaml():
@@ -31,3 +34,57 @@ def test_render_template__with_simple_calculation():
     result = render_template(yaml_content)
 
     assert yaml.safe_load(result) == expected
+
+
+def test_render_template__with_env_variable():
+    os.environ["DCE_DATASOURCE_PASSWORD"] = "mypassword"
+
+    yaml_content = """
+    attribute:
+        subattribute: "test"
+        second_attribute: {{ 123 + 1 }}
+    secret: {{ env_var('DCE_DATASOURCE_PASSWORD') }}
+    """
+
+    expected = {"attribute": {"subattribute": "test", "second_attribute": 124}, "secret": "mypassword"}
+
+    result = render_template(yaml_content)
+
+    assert yaml.safe_load(result) == expected
+
+
+def test_render_template__with_env_variable_default():
+    os.environ["DCE_DATASOURCE_PASSWORD"] = "mypassword"
+
+    yaml_content = """
+    attribute:
+        subattribute: "test"
+        second_attribute: {{ 123 + 1 }}
+    user: {{ env_var('DCE_DATASOURCE_USER', 'default_user') }}
+    secret: {{ env_var('DCE_DATASOURCE_PASSWORD', 'default_password') }}
+    """
+
+    expected = {
+        "attribute": {"subattribute": "test", "second_attribute": 124},
+        "user": "default_user",
+        "secret": "mypassword",
+    }
+
+    result = render_template(yaml_content)
+
+    assert yaml.safe_load(result) == expected
+
+
+def test_render_template__fails_if_env_variable_missing_and_no_default():
+    os.environ["DCE_DATASOURCE_PASSWORD"] = "mypassword"
+
+    yaml_content = """
+    attribute:
+        subattribute: "test"
+        second_attribute: {{ 123 + 1 }}
+    user: {{ env_var('DCE_DATASOURCE_USER') }}
+    secret: {{ env_var('DCE_DATASOURCE_PASSWORD', 'default_password') }}
+    """
+
+    with pytest.raises(UnknownEnvVarTemplateError):
+        render_template(yaml_content)
