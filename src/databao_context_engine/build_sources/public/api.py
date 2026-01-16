fb960245f3bf91ebb39cd5aa3b315d9ad281ff39
Julien Arzul <julien.arzul@jetbrains.com>
2026-01-16 16:43:41 +0100

    Add a new DatabaoContextProjectManager that can build all datasources in the project

--- src/databao_context_engine/build_sources/public/api.py
+++ src/databao_context_engine/build_sources/public/api.py
@@ -1,3 +1,4 @@
+from databao_context_engine.build_sources.internal.build_runner import BuildContextResult
 from databao_context_engine.build_sources.internal.build_wiring import build_all_datasources
 
-__all__ = ["build_all_datasources"]
+__all__ = ["build_all_datasources", "BuildContextResult"]
