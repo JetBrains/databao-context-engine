b6f164e84d13c109170ed67ebd0d98dda00a7cbd
Julien Arzul <julien.arzul@jetbrains.com>
2026-01-19 15:30:28 +0100

    Add DatabaoContextProjectManager in the direct public API of databao_context_engine

--- src/databao_context_engine/__init__.py
+++ src/databao_context_engine/__init__.py
@@ -1,7 +1,9 @@
 from databao_context_engine.build_sources.internal.build_runner import BuildContextResult
-from databao_context_engine.databao_context_project_manager import DatabaoContextProjectManager
+from databao_context_engine.databao_context_project_manager import DatabaoContextProjectManager, DatasourceConfigFile
 from databao_context_engine.databao_engine import ContextSearchResult, DatabaoContextEngine
 from databao_context_engine.datasource_config.datasource_context import DatasourceContext
+from databao_context_engine.datasource_config.validate_config import CheckDatasourceConnectionResult
+from databao_context_engine.pluginlib.build_plugin import DatasourceType
 from databao_context_engine.project.types import Datasource, DatasourceId
 from databao_context_engine.services.chunk_embedding_service import ChunkEmbeddingMode
 
@@ -14,4 +16,7 @@ __all__ = [
     "DatabaoContextProjectManager",
     "ChunkEmbeddingMode",
     "BuildContextResult",
+    "CheckDatasourceConnectionResult",
+    "DatasourceConfigFile",
+    "DatasourceType",
 ]
