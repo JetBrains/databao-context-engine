1746eb010a9efcdde8ae6a9b90d72d2ad40950d6
Julien Arzul <julien.arzul@jetbrains.com>
2026-01-19 10:38:20 +0100

    Add a DatabaoContextProjectManager.create_datasource_config

--- src/databao_context_engine/databao_context_project_manager.py
+++ src/databao_context_engine/databao_context_project_manager.py
@@ -1,18 +1,30 @@
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Any
 
 from databao_context_engine.build_sources.public.api import BuildContextResult, build_all_datasources
+from databao_context_engine.datasource_config.add_config import create_datasource_config_file
 from databao_context_engine.datasource_config.validate_config import (
     CheckDatasourceConnectionResult,
     validate_datasource_config,
 )
+from databao_context_engine.pluginlib.build_plugin import DatasourceType
 from databao_context_engine.project.datasource_discovery import DatasourceId
+from databao_context_engine.project.layout import ensure_project_dir
 from databao_context_engine.services.chunk_embedding_service import ChunkEmbeddingMode
+
+
+@dataclass
+class DatasourceConfigFile:
+    datasource_id: DatasourceId
+    config_file_path: Path
 
 
 class DatabaoContextProjectManager:
     project_dir: Path
 
     def __init__(self, project_dir: Path) -> None:
+        ensure_project_dir(project_dir=project_dir)
         self.project_dir = project_dir
 
     def build_context(
@@ -27,4 +39,20 @@ class DatabaoContextProjectManager:
         return sorted(
             validate_datasource_config(project_dir=self.project_dir, datasource_ids=datasource_ids).values(),
             key=lambda result: str(result.datasource_id),
+        )
+
+    def create_datasource_config(
+        self, datasource_type: DatasourceType, datasource_name: str, config_content: dict[str, Any]
+    ) -> DatasourceConfigFile:
+        # TODO: Before creating the datasource, validate the config content based on which plugin will be used
+        config_file_path = create_datasource_config_file(
+            project_dir=self.project_dir,
+            datasource_type=datasource_type,
+            datasource_name=datasource_name,
+            config_content=config_content,
+        )
+
+        return DatasourceConfigFile(
+            datasource_id=DatasourceId.from_datasource_config_file_path(config_file_path),
+            config_file_path=config_file_path,
         )
