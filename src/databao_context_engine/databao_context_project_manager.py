3518de158f90283c3714b235d8b735d6c2608012
Julien Arzul <julien.arzul@jetbrains.com>
2026-01-20 14:14:00 +0100

    Add a DatabaoContextProjectManager.check_datasource_connection (#72)
    # What?
    This PR adds the new method check_datasource_connection in DatabaoContextProjectManager

--- src/databao_context_engine/databao_context_project_manager.py
+++ src/databao_context_engine/databao_context_project_manager.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 
 from databao_context_engine.build_sources.public.api import BuildContextResult, build_all_datasources
+from databao_context_engine.datasource_config.validate_config import (
+    CheckDatasourceConnectionResult,
+    validate_datasource_config,
+)
 from databao_context_engine.project.datasource_discovery import DatasourceId
 from databao_context_engine.services.chunk_embedding_service import ChunkEmbeddingMode
 
@@ -16,3 +20,11 @@ class DatabaoContextProjectManager:
     ) -> list[BuildContextResult]:
         # TODO: Filter which datasources to build by datasource_ids
         return build_all_datasources(project_dir=self.project_dir, chunk_embedding_mode=chunk_embedding_mode)
+
+    def check_datasource_connection(
+        self, datasource_ids: list[DatasourceId] | None
+    ) -> list[CheckDatasourceConnectionResult]:
+        return sorted(
+            validate_datasource_config(project_dir=self.project_dir, datasource_ids=datasource_ids).values(),
+            key=lambda result: str(result.datasource_id),
+        )
