e9989a33b20ba8874f86f4118703fd7e69043ee3
Julien Arzul <julien.arzul@jetbrains.com>
2026-01-23 16:10:27 +0100

    Add documentation on DatabaoContextProjectManager

--- src/databao_context_engine/databao_context_project_manager.py
+++ src/databao_context_engine/databao_context_project_manager.py
@@ -23,18 +23,47 @@ from databao_context_engine.services.chunk_embedding_service import ChunkEmbeddi
 
 @dataclass
 class DatasourceConfigFile:
+    """A datasource config file that was created by the DatabaoContextProjectManager.
+
+    Attributes:
+        datasource_id: The unique identifier for the datasource.
+        config_file_path: The path to the datasource configuration file.
+    """
+
     datasource_id: DatasourceId
     config_file_path: Path
 
 
 class DatabaoContextProjectManager:
+    """Project Manager for Databao Context Projects.
+
+    This project manager is responsible for configuring and building a Databao Context Project.
+    The project_dir should already have been initialised before a Project manager can be used.
+
+    Attributes:
+        project_dir: The root directory of the Databao Context Project.
+    """
+
     project_dir: Path
 
     def __init__(self, project_dir: Path) -> None:
+        """Initialise the DatabaoContextProjectManager.
+
+        Args:
+            project_dir: The root directory of the Databao Context Project.
+        """
         ensure_project_dir(project_dir=project_dir)
         self.project_dir = project_dir
 
     def get_configured_datasource_list(self) -> list[Datasource]:
+        """Return the list of datasources configured in the project.
+
+        This method returns all datasources configured in the src folder of the project,
+        no matter whether the datasource configuration is valid or not.
+
+        Returns:
+            The list of datasources configured in the project.
+        """
         return get_datasource_list(self.project_dir)
 
     def build_context(
@@ -42,12 +71,31 @@ class DatabaoContextProjectManager:
         datasource_ids: list[DatasourceId] | None = None,
         chunk_embedding_mode: ChunkEmbeddingMode = ChunkEmbeddingMode.EMBEDDABLE_TEXT_ONLY,
     ) -> list[BuildContextResult]:
+        """Build the context for datasources in the project.
+
+        Any datasource with an invalid configuration will be skipped.
+
+        Args:
+            datasource_ids: The list of datasource ids to build. If None, all datasources will be built.
+            chunk_embedding_mode: The mode to use for chunk embedding.
+
+        Returns:
+            The list of all built results.
+        """
         # TODO: Filter which datasources to build by datasource_ids
         return build_all_datasources(project_dir=self.project_dir, chunk_embedding_mode=chunk_embedding_mode)
 
     def check_datasource_connection(
         self, datasource_ids: list[DatasourceId] | None = None
     ) -> list[CheckDatasourceConnectionResult]:
+        """Check the connection for datasources in the project.
+
+        Args:
+            datasource_ids: The list of datasource ids to check. If None, all datasources will be checked.
+
+        Returns:
+            The list of all connection check results, sorted by datasource id.
+        """
         return sorted(
             check_datasource_connection_internal(project_dir=self.project_dir, datasource_ids=datasource_ids).values(),
             key=lambda result: str(result.datasource_id),
@@ -60,6 +108,18 @@ class DatabaoContextProjectManager:
         config_content: dict[str, Any],
         overwrite_existing: bool = False,
     ) -> DatasourceConfigFile:
+        """Create a new datasource configuration file in the project.
+
+        Args:
+            datasource_type: The type of the datasource to create.
+            datasource_name: The name of the datasource to create.
+            config_content: The content of the datasource configuration. This is a dictionary that will be written as-is in a yaml file.
+                The actual content of the configuration is not checked and might not be valid for the requested type..
+            overwrite_existing: Whether to overwrite an existing datasource configuration file if it already exists.
+
+        Returns:
+            The path to the created datasource configuration file.
+        """
         # TODO: Before creating the datasource, validate the config content based on which plugin will be used
         config_file_path = create_datasource_config_file(
             project_dir=self.project_dir,
@@ -86,6 +146,19 @@ class DatabaoContextProjectManager:
         datasource_name: str | None = None,
         datasource_id: DatasourceId | None = None,
     ) -> bool:
+        """Check if a datasource configuration file already exists in the project.
+
+        Args:
+            datasource_type: The type of the datasource. This must be used in conjunction with datasource_name.
+            datasource_name: The name of the datasource. This must be used in conjunction with datasource_type.
+            datasource_id: The id of the datasource. If provided, datasource_type and datasource_name will be ignored.
+
+        Returns:
+            True if there is already a datasource configuration file for this datasource, False otherwise.
+
+        Raises:
+            ValueError: If the wrong set of arguments is provided.
+        """
         if datasource_type is not None and datasource_name is not None:
             datasource_id = get_datasource_id_for_config_file(datasource_type, datasource_name)
         elif datasource_id is None:
