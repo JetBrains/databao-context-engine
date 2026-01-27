56a1b89fc0b9d4a2ef77abee463210797aa61c20
Julien Arzul <julien.arzul@jetbrains.com>
2026-01-27 16:23:51 +0100

    Refact: Inline tools in the MCP Server
    Now that we are directly using functions of the DatabaoContextEngine, the implementation of the tool is very straight-forward and doesn't need to be extracted

--- src/databao_context_engine/mcp/all_results_tool.py
+++
@@ -1,5 +0,0 @@
-from databao_context_engine import DatabaoContextEngine
-
-
-def run_all_results_tool(databao_context_engine: DatabaoContextEngine) -> str:
-    return databao_context_engine.get_all_contexts_formatted()
