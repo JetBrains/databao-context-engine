ea8d55f953387e3d9c00fff907d00dce9a3822bf
Julien Arzul <julien.arzul@jetbrains.com>
2026-01-27 10:18:40 +0100

    Add the sort import rule in our Ruff configuration (#89)
    # What?
    
    This PR adds a rule in our Ruff configuration to make sure our imports
    are sorted.
    
    # Why?
    
    Currently, imports that I haven't touched get shuffled around when I use
    the "Optimize Imports" in PyCharm. It's not a huge deal but it would
    make our Python file more consistent over time if we were applying this
    rule from the beginning
    
    # How to avoid getting a linter error?
    
    Unfortunately, formatting the file is not enough to sort the imports:
    this rule was added to ruff as part of the linter and not the formatter.
    It means that we have two easy solutions to always make sure the imports
    are sorted:
    1. run `uv run ruff check --fix` whenever we get the linter error in a
    pre-commit file
    2. configure PyCharm to "Optimize Imports" on file saves

--- src/databao_context_engine/plugins/databases/snowflake_introspector.py
+++ src/databao_context_engine/plugins/databases/snowflake_introspector.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Dict, List, Annotated
+from typing import Annotated, Any, Dict, List
 
 import snowflake.connector
 from pydantic import BaseModel, Field
