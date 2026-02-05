ad0ef1fdaa04fd4fe83b564233705c1a8d4d2c61
MateusCordeiro <mateusvcordeiro@gmail.com>
2026-02-05 11:42:32 +0100

    mypy fixes

--- src/databao_context_engine/pluginlib/build_plugin.py
+++ src/databao_context_engine/pluginlib/build_plugin.py
@@ -20,7 +20,7 @@ class EmbeddableChunk:
 class BaseBuildPlugin(Protocol):
     id: str
     name: str
-    context_type: type[Any] | None
+    context_type: type[Any]
 
     def supported_types(self) -> set[str]: ...
 
