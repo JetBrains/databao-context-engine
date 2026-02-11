7fc76404acb8ddf6dcecfd2e6885e0c1682c3f7e
Julien Arzul <julien.arzul@jetbrains.com>
2026-02-11 09:58:54 +0100

    Make sure to always ignore the all_results.yaml file when looking for datasource contexts

--- src/databao_context_engine/datasources/types.py
+++ src/databao_context_engine/datasources/types.py
@@ -95,7 +95,7 @@ class DatasourceId:
         Returns:
             The absolute path to the config file.
         """
-        return project_layout.src_dir / (self.datasource_path + self.config_file_suffix)
+        return project_layout.src_dir / self.relative_path_to_config_file()
 
     def relative_path_to_context_file(self) -> Path:
         """Return a path to the config file for this datasource.
@@ -113,6 +113,17 @@ class DatasourceId:
         )
 
         return Path(self.datasource_path + suffix)
+
+    def absolute_path_to_context_file(self, project_layout: ProjectLayout) -> Path:
+        """Return an absolute path to the context file for this datasource.
+
+        Args:
+            project_layout: The databao context engine project layout.
+
+        Returns:
+            The absolute path to the context file.
+        """
+        return project_layout.output_dir / self.relative_path_to_context_file()
 
     @classmethod
     def from_string_repr(cls, datasource_id_as_string: str) -> "DatasourceId":
