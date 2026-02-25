a311a587e6f3085a2eebe0d609c158312e36cc3e
Sergey Stupin <stupin.sergey@gmail.com>
2026-02-25 16:56:53 +0100

    fix space

--- src/databao_context_engine/datasources/config_wizard.py
+++ src/databao_context_engine/datasources/config_wizard.py
@@ -87,7 +87,7 @@ def _build_config_content_from_properties(
                 config_content[config_file_property.property_key] = nested_content
         else:
             property_value = user_input_callback.prompt(
-                text=f"{properties_prefix}{config_file_property.property_key}? {'(Optional)' if not config_file_property.required else ''}",
+                text=f"{properties_prefix}{config_file_property.property_key}?{' (Optional)' if not config_file_property.required else ''}",
                 type=config_file_property.property_type,
                 default_value=config_file_property.default_value,
                 is_secret=config_file_property.secret,
